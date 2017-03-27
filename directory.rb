eed4533d24939a6b9670ad7e5987e9e66a87279b
Robert Jones <robert.g.jones@gmail.com>
2017-03-27 17:33:17 +0100

    Add students to array, print names with iterator

--- directory.rb
+++ directory.rb
@@ -1,18 +1,25 @@
-student_count = 12
-#print list of students
-puts "The student of Villains Academy"
+#put students into an array
+students = [
+   "Dr. Hannibal Lecter",
+   "Darth Vader",
+   "Nurse Ratched",
+   "Michael Corleone",
+   "Alex DeLarge",
+   "The Wicked Witch of the West",
+   "Terminator",
+   "Freddy Krueger",
+   "The Joker",
+   "Joffrey Baratheon",
+   "Norman Bates",
+   "Michael Gove",
+ ]
+
+#print student names
+puts "The students of Villains Academy"
 puts "-" * 15
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-puts "Michael Gove"
+students.each do |student|
+  puts student
+end
+
 #print the number of students
-puts "Overall, we have #{student_count} great students"
+puts "Overall, we have #{students.count} great students"
