fb2de6f7d41b5b2431931020969087c30cb59c83
Robert Jones <robert.g.jones@gmail.com>
2017-03-28 00:35:35 +0100

    Add while loop

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+#captures student names and inputs to an array of hashes
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
@@ -17,38 +18,59 @@ def input_students
   students
 end
 
+#def search_criteria
+#end
+
 def print_header
   puts "The students of Rhubarb Academy"
   puts "-" * 15
 end
 
-def print(students)
-  students.each_with_index do |student, idx|
-    puts "#{idx+1}. #{student[:name]} (#{student[:cohort]} cohort)"
-  end
-end
 # captures which letter the user wants to search by
-def print_specific_header
+def print_with_letter_header
   puts "What letter would you like to search for pupils by?"
   letter = gets.chomp.upcase
   puts "The Students of Rhubarb Academy with names beginning with #{letter}"
   puts '-' * 15
 end
-#Returns pupils only if they meet the condition in the block, which is the first letter of their name.
-def print_specific(students)
+
+# prints all registered student with index position
+def print(students)
   students.each_with_index do |student, idx|
-    puts "#{idx+1}. #{student[:name]} (#{student[:cohort]} cohort)" if student[:name].start_with?("R", "r")
+    puts "#{idx+1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(students)
-  puts "Overall, we have #{names.count} great students"
+# prints all students using a while loop
+def print_while(students)
+  count = 0
+    until count == students.count do
+      students.each_with_index do |student, idx|
+        puts "#{idx+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+        count += 1
+      end
+    end
 end
 
-#nothing will happen unless we call the methods
+# Returns pupils only if they meet the condition in the block, which is the first letter of their name.
+def print_with_letter(students, letter)
+  students.each_with_index do |student, idx|
+    puts "#{idx+1}. #{student[:name]} (#{student[:cohort]} cohort)" if student[:name].start_with?(letter)
+  end
+end
+
+# prints total num of students on roll
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
+end
+
+# method calls
+first_letter = "R"
 students = input_students
 print_header
 print(students)
-print_specific_header
-print_specific(students)
+print_header
+print_while(students)
+print_with_letter_header
+print_with_letter(students, first_letter)
 print_footer(students)
