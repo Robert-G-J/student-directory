11a2e7650347db7e86118ecd0a26ad4aef5ffed5
Robert Jones <robert.g.jones@gmail.com>
2017-03-28 14:22:07 +0100

    Add extra keys to student info

---
+++ default_array.rb
@@ -0,0 +1,5 @@
+students = [
+{name: 'Donald Trump', cohort: :January, gender: :male, hobbies: "Running", height: "4\'3\""}
+{name: 'Jeremy Corbyn', cohort: :November, gender: :male, hobbies: "Whinging", height: "5\'10\""}
+
+]
