387d79a41c01c4bb283fc086d032f0ca3333550e
Robert Jones <robert.g.jones@gmail.com>
2017-03-28 21:24:47 +0100

    Add array of extended student details

--- default_array.rb
+++ default_array.rb
@@ -1,5 +1,15 @@
+#students = [
+#{name: 'Donald Trump', cohort: :January, gender: :male, hobbies: "Running", height: "4\'3\""},
+#{name: 'Jeremy Corbyn', cohort: :November, gender: :male, hobbies: "Whinging", height: "5\'10\""}]
 students = [
-{name: 'Donald Trump', cohort: :January, gender: :male, hobbies: "Running", height: "4\'3\""}
-{name: 'Jeremy Corbyn', cohort: :November, gender: :male, hobbies: "Whinging", height: "5\'10\""}
-
-]
+ {name: "Dr. Hannibal Lecter", cohort: 'november', hobbies: "football", date_of_birth: 'june,1990', gender: 'masculin', height: '6foot'},
+ {name: "Darth Vadera", cohort: :May, hobbies: "boxing", date_of_birth: "july,1987", gender: "masculin", height: "6\'0 foot" },
+ {name: "Nurse Ratched", cohort: :June ,hobbies: "surfing", date_of_birth: "august,1987", gender: "masculin", height: "6\'1 foot"},
+ {name: "Michael Corleone", cohort: :May, hobbies: "reading", date_of_birth: "september,1985", gender: "masculin", height: "5\'9 foot"},
+ {name: "Alex DeLarge", cohort: :December, hobbies: "swimming", date_of_birth: "july,1984", gender: "masculin", height: "6\'4 foot"},
+ {name: "The Wicked Witch of the West", cohort: 'July', hobbies: "running", date_of_birth: "july,1989", gender: "feminin", height: "5\'9 foot"},
+ {name: "Terminator", cohort: :October, hobbies: "running", date_of_birth: "july,1989", gender: "masculin", height: "6\'9 foot"},
+ {name: "Freddy Krueger", cohort: 'november', hobbies: "programming", date_of_birth: "july,1987", gender: "masculin", height: "6\'3 foot"},
+ {name: "The Joker", cohort: 'november', hobbies: "running", date_of_birth: "july,1989", gender: "masculin", height: "6\'2 foot"},
+ {name: "Joffrey Baratheon", cohort: 'november', hobbies: "kick_boxing", date_of_birth: "july,1987", gender: "masculin", height: "6\'5 foot"},
+ {name: "Norman Bates", cohort: 'november', hobbies: "running", date_of_birth: "july,1990", gender: "masculin", height: "6\'7 foot"}]
