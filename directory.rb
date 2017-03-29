5859195a9270a42f29b7698a90796072d66a4e18
Robert Jones <robert.g.jones@gmail.com>
2017-03-29 11:31:57 +0100

    Refactored interactive menu by using a class variable and splitting into smaller methods

--- directory.rb
+++ directory.rb
@@ -1,56 +1,57 @@
+@students = [] # an empty array
+
 def interactive_menu
-  students = []
+  #students = []
   loop do
-    # 1. show user a list of options
-    puts "1. Input new students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    # 2. ask the user what to do
-    selection = gets.chomp
-    # 3. execute user request
-    case selection
-    when "1"
-      students = input_students
-    when "2"
-      print_header
-      print_all(students)
-      print_footer(students)
-    when "9"
-      exit
-    else
-      puts "I don't know what you meant, try again."
-    end
+    print_menu
+    process(gets.chomp)
   end
 end
 
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you meant, try again."
+  end
+end
 
+def print_menu
+  puts "1. Input new students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
 
-
-
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
 
 def input_students
-  puts "ADD STUDENT INFORMATION:"
   puts "Please enter the information for each student."
   puts "To finish, ignore options and hit return. "
-  # create an empty array
-  students = []
   # get the first name
   name = gets.strip #strip
-
-  puts "\nPlease enter the month"
+  puts "Please enter the month"
   month = gets.chomp
   while !name.empty? do
 
         if !month.empty?
-            students << {name: name, cohort: month}
+            @students << {name: name, cohort: month}
         else
-            students << {name: name, cohort: :UNKNOWN}
+            @students << {name: name, cohort: :UNKNOWN}
         end
 
-        if students.count > 1
-          puts "\nWe now have #{students.count} students."
+        if @students.count > 1
+          puts "\nWe now have #{@students.count} students."
         else
-          puts "\nWe now have #{students.count} student."
+          puts "\nWe now have #{@students.count} student."
         end
       puts "-" * $lineWidth
       puts "\nPlease enter another name or hit return twice"
@@ -58,7 +59,6 @@ def input_students
       puts "Enter another month"
       month = gets.chomp
   end
-  students
 end
 
 #def search_criteria
@@ -88,17 +88,17 @@ def get_char
   end
 
 # prints all registered student with index position
-def print_all(students)
-  students.each_with_index do |student, idx|
+def print_student_list
+  @students.each_with_index do |student, idx|
     puts "#{idx+1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
 # prints all students using a while loop
-def print_while(students)
+def print_while
   count = 0
-    until count == students.count do
-      students.each_with_index do |student, idx|
+    until count == @students.count do
+      @students.each_with_index do |student, idx|
         puts "#{idx+1}. #{student[:name]}" + "(#{student[:cohort]} cohort)".rjust(40)
         count += 1
       end
@@ -106,35 +106,35 @@ def print_while(students)
 end
 
 # Returns pupils only if they meet the condition in the block, which is the first letter of their name.
-def print_with_letter(students, letter)
-  students.each do |student|
+def print_with_letter(letter)
+  @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)" if student[:name].start_with?(letter)
   end
 end
 
 # Returns students with names under a user determined number of characters
-def print_char(students, char)
-  students.each do |student|
+def print_char(char)
+  @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)" if student[:name].length < char
   end
 
 end
 
-def print_by_cohort(students)
-  cohorts = students.map {|student| student[:cohort]}.uniq
+def print_by_cohort
+  cohorts = @students.map {|student| student[:cohort]}.uniq
   cohorts.each do |month|
-    students.each do |student|
+    @students.each do |student|
       puts "#{student[:name]} (#{student[:cohort]} cohort)" if student[:cohort] == month
     end
   end
 end
 
 # prints total num of students on roll
-def print_footer(students)
+def print_footer
   puts "-" * $lineWidth
-  if students.count > 1
-    puts "We have #{students.count} great students in total. "
-  elsif students.count == 1
+  if @students.count > 1
+    puts "We have #{@students.count} great students in total. "
+  elsif @students.count == 1
     puts "We have 1 great student. "
   else
     puts "We have zero students in the directory"
@@ -149,12 +149,12 @@ info_keys = [:surname, :forename, :cohort, :DOB, :gender]
 
 # method calls
 interactive_menu
-#students = input_students
+
 #print_header
-#print_all(students)
+#print_all(@students)
 #print_header
-#print_while(students)
-#print_with_letter(students, get_letter)
-#print_char(students, get_char)
-#print_by_cohort(students)
-#print_footer(students)
+#print_while(@students)
+#print_with_letter(@students, get_letter)
+#print_char(@students, get_char)
+#print_by_cohort(@students)
+#print_footer(@students)
