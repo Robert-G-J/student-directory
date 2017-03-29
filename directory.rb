d33115a555d81fa070bd6cf629d79b33d169fdbb
Robert Jones <robert.g.jones@gmail.com>
2017-03-29 13:30:30 +0100

    Refactored. Use method to populate global student info array

--- directory.rb
+++ directory.rb
@@ -1,10 +1,18 @@
-@students = [] # an empty array
+@students = [] # an empty array accessible to all methods
+
+def print_menu
+  puts "1. Input new students"
+  puts "2. Show the students"
+  puts "3. Save the student-list to student.csv"
+  puts "4. Load student-list from students.csv"
+  puts "9. Exit"
+end
 
 def interactive_menu
   #students = []
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
@@ -25,14 +33,6 @@ def process(selection)
   end
 end
 
-def print_menu
-  puts "1. Input new students"
-  puts "2. Show the students"
-  puts "3. Save the student-list to student.csv"
-  puts "4. Load student-list from students.csv"
-  puts "9. Exit"
-end
-
 def show_students
   print_header
   print_student_list
@@ -43,15 +43,15 @@ def input_students
   puts "Please enter the information for each student."
   puts "To finish, ignore options and hit return. "
   # get the first name
-  name = gets.strip #strip
+  name = STDIN.gets.strip #strip
   puts "Please enter the month"
-  month = gets.chomp
+  month = STDIN.gets.chomp
   while !name.empty? do
 
         if !month.empty?
-            @students << {name: name, cohort: month}
+            populate_student_info_array(name, month)
         else
-            @students << {name: name, cohort: :UNKNOWN}
+            populate_student_info_array(name, :unknown)
         end
 
         if @students.count > 1
@@ -61,14 +61,15 @@ def input_students
         end
       puts "-" * $lineWidth
       puts "\nPlease enter another name or hit return twice"
-      name = gets.chomp
+      name = STDIN.gets.chomp
       puts "Enter another month"
-      month = gets.chomp
+      month = STDIN.gets.chomp
   end
 end
 
-#def search_criteria
-#end
+def populate_student_info_array(name, month)
+  @students << {name: name, cohort: month.to_sym}
+end
 
 def print_header
   puts "\nThe Students of Rhubarb Academy".center($lineWidth)
@@ -78,7 +79,7 @@ end
 # captures which letter the user wants to search by
 def get_letter
   puts "What letter would you like to search for pupils by?"
-  first_letter = gets.chomp.upcase
+  first_letter = STDIN.gets.chomp.upcase
   puts "The Students of Rhubarb Academy with names beginning with #{first_letter}"
   puts '-' * $lineWidth
   first_letter
@@ -87,7 +88,7 @@ end
 # Obtain number of characters to search for
 def get_char
     puts "Search for names of characters less than:"
-    char = gets.chomp.to_i
+    char = STDIN.gets.chomp.to_i
     puts "The Students of Rhubarb Academy with names shorter than #{char}"
     puts '-' * $lineWidth
     char
@@ -147,12 +148,25 @@ def save_students
   file.close
 end
 
-def load_students
-  file = File.open("students.csv", "r")
+def try_load_students
+  filename = ARGV.first # first argument from the commandline
+  return if filename.nil? # get out of method if not given
+  if File.exists?(filename) # if it exists
+    load_students(filename)
+    puts "Loaded #{@students.count} from file: #{filename}"
+  else # if it didn't exist
+    puts "Sorry, #{filename} doesn't exist."
+    exit #quits program
+  end
+end
+
+
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(',') # makes an array from each line, split at each comma. Before the comma goes
-    @students << {name: name, cohort: cohort.to_sym}
-  end
+    populate_student_info_array(name, cohort.to_sym)
+    end
   file.close
 end
 
@@ -175,6 +189,7 @@ $columnWidth = 15
 info_keys = [:surname, :forename, :cohort, :DOB, :gender]
 
 # method calls
+try_load_students
 interactive_menu
 
 #print_header
